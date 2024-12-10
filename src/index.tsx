e3e6b0d9fcd896b1c9975d9a486496b2d79576fa
junseong <ahn990423@gmail.com>
2024-12-11 03:38:07 +0900

    perf: update react to 19

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { initializeWASM } from "./wasm/initalizeWASM";
+import CreateDOM from 'react-dom/client';
 
 
 const startApp = async () => {
   // Initialize WASM before rendering the app
   await initializeWASM();
-
+  const rootElement = document.getElementById('root');
+  if (!rootElement) throw new Error('Failed to find the root element');
+  
   // Now render the App once WASM is initialized
-  ReactDOM.render(<App />, document.getElementById('root'));
+  const root = CreateDOM.createRoot(rootElement);
+
+  root.render(<App />);
 };
 
 // Start the app
